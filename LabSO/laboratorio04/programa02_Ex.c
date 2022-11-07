342aa934631e2172c49ddc5c0400194bd02a8968
jordany <jordanyluiz@gmail.com>
2022-11-07 00:25:42 -0300

    adding works to laboratorio04

--- LabSO/laboratorio04/programa02_Ex.c
+++ LabSO/laboratorio04/programa02_Ex.c
@@ -2,51 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <semaphore.h>
-#include <fcntl.h>
 
-#define MAX_COUNT 500
+#define MAX_COUNT 32
 
-
-void ChildProcess(sem_t*);
-void ParentProcess(sem_t*);
+void ChildProcess(void);
+void ParentProcess(void);
 
 void main(void)
-{   
-    sem_t *mutex = sem_open("mutex", O_CREAT|O_EXCL, O_WRONLY| O_CREAT| O_RDONLY| O_TRUNC, 1);    
+{
     pid_t pid;
-
     pid = fork();
-    if (pid == 0){
-        ChildProcess(mutex);
-    }else{
-        ParentProcess(mutex);
-    }
-
-    sem_unlink("mutex");
+    if (pid == 0)
+        ChildProcess();
+    else
+        ParentProcess();
     exit(0);
 }
 
-void ChildProcess(sem_t* mutex)
-{   
-    sem_wait(mutex);
-
+void ChildProcess(void)
+{
     int i;
     for (i = 1; i <= MAX_COUNT; i++)
         printf(" Processo FILHO, valor = %d\n", i);
     printf(" *** Processo FILHO finalizado ***\n");
-
-    sem_post(mutex);
 }
 
-void ParentProcess(sem_t* mutex)
-{   
-    sem_wait(mutex);
-
+void ParentProcess(void)
+{
     int i;
     for (i = 1; i <= MAX_COUNT; i++)
         printf("Processo PAI, value = %d\n", i);
     printf("*** Processo PAI finalizado ***\n");
-
-    sem_post(mutex);    
+    exit(0);
 }
---
+++ LabSO/laboratorio04/tarefa02.c
@@ -0,0 +1,203 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define FACTORIAL 19
+#define LAST_TERM 100
+#define DISCS_NUMBER 6
+#define BUFF_SIZE 2024
+
+int calculateFactorialOf(int);
+void fibonacciSequenceTo(int);
+void towerOfHanoi(int);
+void hanoi(void);
+int posA = 0;
+int posB = 0;
+int posC = 0;
+int towerA[DISCS_NUMBER];
+int towerB[DISCS_NUMBER];
+int towerC[DISCS_NUMBER];
+
+
+void main(void){
+    pid_t pid;
+    
+    pid = fork();
+    if (pid == 0){
+        pid = fork();
+        if (pid == 0)
+            hanoi();
+        else
+            fibonacciSequenceTo(LAST_TERM);
+    }else{
+        calculateFactorialOf(FACTORIAL);
+    }
+
+    // hanoi();
+    exit(0);
+}
+
+int calculateFactorialOf(int number){
+    char buff[BUFF_SIZE] = "";
+    char* pnt = buff;
+    int bytesWritten;
+    long factorial = 1;
+
+    for (int i = number; i > 0; i--){
+        factorial *= i; 
+
+        if (i == number){
+            bytesWritten = sprintf(buff, "[PARENT] %d!\n", i);
+        }else if (i != 1){
+            bytesWritten += sprintf(buff+bytesWritten, " . %d!\n", i);
+        }else{
+            bytesWritten += sprintf(buff+bytesWritten, " . %d!\n", i);
+        }
+
+        write(1, buff, strlen(buff));
+        printf("[PARENT] CURRENT VALUE: %ld\n", factorial);
+        strcpy(&pnt[strlen(buff)-1],"\0");
+        strcpy(&pnt[strlen(buff)-1],"\0");
+        bytesWritten -= 2;
+    }
+
+    bytesWritten += sprintf(buff+bytesWritten, " = %ld\n", factorial);
+    write(1, buff, strlen(buff));
+    write(1,"############## FINISHING PARENT ##############\n", 47);
+
+    return factorial;
+}
+
+void fibonacciSequenceTo(int lastTerm){
+    long firstTerm = 1;
+    long nextTerm = 0;
+    long auxTerm = 0;
+    char buff[BUFF_SIZE] = "";
+    char* pnt = buff;
+    int bytesWritten;
+
+    for (int i = 1; i <= lastTerm; i++){
+        auxTerm = nextTerm;
+        nextTerm = auxTerm + firstTerm;
+        firstTerm = auxTerm;
+        printf("[CHILD] T%d: %ld\n", i,nextTerm);
+    }
+
+    write(1,"############## FINISHING CHILD ##############\n", 47);
+    write(1,"\n\n",2);
+}
+
+void pushA(int disc){
+    towerA[posA] = disc;
+    posA++;
+}
+void pushB(int disc){
+    towerB[posB] = disc;
+    posB++;
+}
+void pushC(int disc){
+    towerC[posC] = disc;
+    posC++;
+}
+
+int popA(){
+    posA--;
+    return towerA[posA];
+}
+
+int popB(){
+    posB--;
+    return towerB[posB];
+}
+
+int popC(){
+    posC--;
+    return towerC[posC];
+}
+
+int peekA(){
+    return towerA[posA - 1];
+}
+
+int peekB(){
+    return towerB[posB - 1];
+}
+
+int peekC(){
+    return towerC[posC - 1];
+}
+
+void moveAB(){
+    if (posA == 0){
+        pushA(popB());
+    }else if (posB == 0){
+        pushB(popA());
+    }else if (peekA() > peekB()){
+        pushA(popB());
+    }else if (peekA() < peekB()){
+        pushB(popA());
+    }
+}
+
+void moveBC(){
+    if (posC == 0){
+        pushC(popB());
+    }else if (posB == 0){
+        pushB(popC());
+    }else if (peekC() > peekB()){
+        pushC(popB());
+    }else if (peekC() < peekB()){
+        pushB(popC());
+    }
+}
+
+void moveAC(){
+    if (posA == 0){
+        pushA(popC());
+    }else if (posC == 0){
+        pushC(popA());
+    }else if (peekA() > peekC()){
+        pushA(popC());
+    }else if (peekA() < peekC()){
+        pushC(popA());
+    }
+}
+
+void show(){
+    printf("[GRANDCHILD] Tower A: ");
+    for (int i = 0; i < posA; i++){
+        printf("%d ", towerA[i]);
+    }
+    printf("\n[GRANDCHILD] Tower B: ");
+    for (int i = 0; i < posB; i++){
+        printf("%d ", towerB[i]);
+    }
+    printf("\n[GRANDCHILD] Tower C: ");
+    for (int i = 0; i < posC; i++){
+        printf("%d ", towerC[i]);
+    }
+    printf("\n\n");
+}
+
+void bootHanoi(){
+    for (int i = 6; i >= 1; i--){
+        pushA(i);
+    }
+}
+
+void hanoi(){
+    bootHanoi();
+    show();
+    while(posC < 6){
+        moveAB();
+        show();
+        if (posC == 6) break;
+        moveAC();
+        show();
+        if (posC == 6) break;
+        moveBC();
+        show();
+    }
+}
