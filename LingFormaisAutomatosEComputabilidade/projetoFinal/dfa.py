dd6b104b5f1443c2345447aa4e986b240fa28303
jordany <jordanyluiz@gmail.com>
2022-12-01 17:49:27 -0300

    implementing run function in LFAC/projetoFinal/dfa.py

--- LingFormaisAutomatosEComputabilidade/projetoFinal/dfa.py
+++ LingFormaisAutomatosEComputabilidade/projetoFinal/dfa.py
@@ -1,18 +1,36 @@
 import string
-import copy
 
 class DFA:
-    def __init__(self, alphabet:set, states:set, initialState:str, finalState:str):
+    def __init__(self, alphabet:set, states:set, transitions:dict,initialState:str, finalState:str):
         self.alphabet = alphabet
         self.states = states
+        self.transitions = transitions
         self.initialState = initialState
         self.finalState = finalState 
 
     def setTransitionFunction(self, transitions:dict):
         pass
 
-    def run(self, string):
-        pass
+    def run(self, string:str):
+        __currentState = self.initialState
+
+        for index, character in enumerate(string):
+            try:
+                print(f"{__currentState} --- {character} --->", end=" ")
+                __currentState = self.transitions[__currentState][character]  
+                print(__currentState)  
+            except KeyError as err:
+                print("\n")
+                if not err.args[0]:
+                    print(f"o caracter '{string[index - 1]}' não pertence ao alfabeto")
+                else:
+                    print(f"o caracter '{character}' não pertence ao alfabeto")
+                break
+        
+        if __currentState == self.finalState:
+            print("palavra aceita")
+        else:
+            print("palavra recusada, pois não atinge o estado final")
 
 
 I = {*(string.ascii_letters + "_")}
@@ -20,12 +38,9 @@ M = {*(string.ascii_letters + string.digits + "_")}
 MDI = M.difference(I)
 IUM = I.union(M)
 
-dfa = DFA(
-    alphabet=IUM,
-    states={"q0", "q1", "qf"},
-    initialState="q0",
-    finalState="qf"
-    )
+print(I)
+print(M)
+print(MDI)
 
 transitions = {}
 
@@ -46,3 +61,13 @@ transitions["qf"] = {
                     symbol: None
                     for symbol in M.union({",", ";"})
                     }
+
+dfa = DFA(
+    alphabet=IUM,
+    states={"q0", "q1", "qf"},
+    transitions=transitions,
+    initialState="q0",
+    finalState="qf"
+    )
+
+dfa.run("testeDiferenciado, teste;")
